2cba959434cbd54acc34cddfeb46680afa8e634e
Junya Ishihara <junya@champierre.com>
2013-06-09 23:59:05 +0900

    add s3_host_name

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,6 +1,6 @@
 if Rails.env == "production"
   # set credentials from ENV hash
-  S3_CREDENTIALS = { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'], :bucket => 'mopa'}
+  S3_CREDENTIALS = { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'], :bucket => 'mopa', :s3_host_name => 's3-ap-northeast-1.amazonaws.com'}
 else
   # get credentials from YML file
   S3_CREDENTIALS = Rails.root.join("config/s3.yml")
