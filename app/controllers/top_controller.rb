32779ab7518470d1827eed7e09d5523df3da46f5
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2013-08-12 04:23:40 +0900

    resolved #51

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,8 +1,13 @@
 class TopController < ApplicationController
   skip_before_filter :require_login
   def index
-    @default_lat = Spot.last.lat
-    @default_lng = Spot.last.lng
+    if latest_spot = Spot.last
+      @default_lat = latest_spot.lat
+      @default_lng = latest_spot.lng
+    else
+      @default_lat = 34.809639
+      @default_lng = 135.532306
+    end
   end
 
   def how_to_join
